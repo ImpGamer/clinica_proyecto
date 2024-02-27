2de55d59b316105b7789a3ec8b202115e4d7f66e
ImpDev <leoe25939@gmail.com>
2024-02-27 17:17:19 -0600

    Inicio terminado

---
+++ src/main/java/com/imp/clinica/controllers/AppController.java
@@ -0,0 +1,14 @@
+package com.imp.clinica.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/inicio")
+public class AppController {
+    @GetMapping
+    String inicio() {
+        return "index";
+    }
+}

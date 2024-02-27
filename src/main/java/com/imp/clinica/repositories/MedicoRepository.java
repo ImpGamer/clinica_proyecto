f3a84dc75e5c7d3ec5dd96626e1adb201ac08648
ImpDev <leoe25939@gmail.com>
2024-02-27 14:54:56 -0600

    Repositorios creados

---
+++ src/main/java/com/imp/clinica/repositories/MedicoRepository.java
@@ -0,0 +1,9 @@
+package com.imp.clinica.repositories;
+
+import com.imp.clinica.entities.Medico;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MedicoRepository extends JpaRepository<Medico,Long> {
+}
